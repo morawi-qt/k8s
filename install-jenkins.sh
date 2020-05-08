a705d5a5483e269bbb111d2be695a90f07ad2832
morawi-qt <59061106+morawi-qt@users.noreply.github.com>
2020-05-08 11:49:03 +0100

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -2,6 +2,9 @@
 
 sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+# Must be done to obtain the new repo for Jenkins 
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt install jenkins
 sudo systemctl start jenkins
 sudo systemctl status jenkins
