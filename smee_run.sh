4d20fd4730fadc4d89fc115c2e3e6adea395f91a
morawi-qt <59061106+morawi-qt@users.noreply.github.com>
2020-05-08 12:28:41 +0100

    Create smee_run.sh

---
+++ smee_run.sh
@@ -0,0 +1,11 @@
+
+
+#!/bin/bash
+
+# This connects the local Jenkins to the git hub hooks broker proxy app
+# To run this in bash do:
+# nohup bash -c 'mkdir smee_run.sh &&'
+
+smee -u https://smee.io/bK3oxson2b5T4ny -t 35.178.254.70:3000
+
+exit 0
