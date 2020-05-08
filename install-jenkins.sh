dd30b24f2dc23229dc6f30f962ab9111d26544ab
morawi-qt <59061106+morawi-qt@users.noreply.github.com>
2020-05-08 11:18:07 +0100

    Create install-jenkins.sh
    How to install Jenkins on ubuntu 18.04

---
+++ install-jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+exit 0
